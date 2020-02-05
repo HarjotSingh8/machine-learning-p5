ee9471727c3870b4e62ab8a7b06666faa6dc62d3
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-02-06 01:45:17 +0530

    working on ml algorithm

--- ml.js
+++ ml.js
@@ -16,9 +16,9 @@ class MachineLearning {
    * outputNodes = number of outputs
    * intermediateLayers = array containing number of node in each intermediate layer
    *
-   * weightsAndBalances[2][][]
-   * weights => weightsAndBalances[0]
-   * balances => weightsAndBalances[1]
+   * weightsAndBalances
+   * weights (factor that is multiplied)
+   * biases (factor that is added)
    *
    * Constructor can have 3 or 4 inputs
    * First 3 inputs set inputNodes, outputNodes and intermediateLayers
@@ -38,29 +38,66 @@ class MachineLearning {
    * returns output
    * This funciton takes input, processes it and gives output
    */
-  constructor(inputNodes, outputNodes, intermediateLayers, weightsAndBiases) {
-    this.inputNodes = inputNodes;
-    this.outputNodes = outputNodes;
-    this.intermediateLayers = intermediateLayers;
-    if (weightsAndBiases == null) {
+
+  constructor(inputNodes, outputNodes, intermediateLayers, weights, biases) {
+    this.inputNodes = inputNodes; //number of input nodes
+    this.outputNodes = outputNodes; //number of output nodes
+    this.intermediateLayers = intermediateLayers; //number of intermediate layers
+    if (weights == null || biases == null) {
+      //if arguments for weights and biases are not given, initialise them with random values
       this.generateRandomWeightsAndBiases();
-      console.log("no weights and balances given");
+      //console.log("no weights and balances given");
     } else {
-      this.setWeightsAndBiases(weightsAndBiases);
-      console.log("weights and balances given");
+      //if arguments for weights and balances are given, copy those values
+      this.setWeightsAndBiases(weights, biases);
+      //console.log("weights and balances given");
     }
   }
   generateRandomWeightsAndBiases() {
     /* generate random weights and biases for the first generation */
+    this.weights = [];
+    this.balances = [];
+    for (let i = 0; i < intermediateLayers.length; i++) {
+      let layerWeights = [];
+      let layerBalances = [];
+      for (let j = 0; j < intermediateLayers[i]; j++) {
+        layerWeights.push(random(-10, 10));
+        layerBalances.push(random(-10, 10));
+      }
+      this.weights.push(layerWeights);
+      this.balances.push(layerBalances);
+    }
   }
-  setWeightsAndBiases(weightsAndBiases) {
+  setWeightsAndBiases(weights, biases) {
     /* set predefined weights and balances */
+    this.weights = weights;
+    this.biases = biases;
   }
   mutateWeightsAndBalances() {
     /* add variations to weights and balances in subsequent generations */
   }
   process(inputs) {
-    for (let i = 0; i < numChildren; i++) {}
+    /*this is just a driver function for the process layer recursive function*/
+    return this.processLayer(0, inputs);
+  }
+  processLayer(layer, inputs) {
+    /*recursive function that performs all the calculation with current weights and biases*/
+    let values = [];
+    for (let i = 0; i < intermediateLayers[layer].length; i++) {
+      /*initialise values array with zeroes*/
+      values.push(0);
+    }
+    for (let i = 0; i < inputs.length; i++) {
+      for (let j = 0; j < intermediateLayers[layer].length; j++) {
+        /*multiplying weights with inputs and adding biases for every node in inupt and node in layer*/
+        values[j] = inputs[i] * this.weights[layer][j] + this.biases[layer][j];
+      }
+    }
+    if (layer == intermediateLayers.length) {
+      return output;
+    } else {
+      return this.processLayer(layer + 1, values);
+    }
   }
 }
 
